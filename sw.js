39d9f69d11592efc0b433f6452cda4d5751557a0
Trebzy <rkulibert@gmail.com>
2026-09-12 11:19:23 -0700

    Chromebook-first QuestLab PWA for Solvay MS

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="questlab-cb-v1";const FILES=["./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
